a2909ca8fcebbcc64887b7a97b165cbb4510c8b4
Priyanshu <priyanshu57005@gmail.com>
2025-09-27 12:12:31 +0530

    proj

---
+++ src/components/Tours.js
@@ -0,0 +1,19 @@
+import Card from "./Card";
+
+
+function Tours({tours, removeTour}){
+    return(
+        <div className="container">
+            <h2 className="title">Plan with Priyanshu</h2>
+            <div className="cards">
+                {
+                    tours.map( (tour)=>{
+                        return <Card {...tour} removeTour={removeTour}></Card>
+                    })
+                }
+            </div>
+        </div>
+    )
+}
+
+export default Tours;
